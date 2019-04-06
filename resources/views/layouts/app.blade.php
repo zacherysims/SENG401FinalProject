49e0e41011e3242a96688648396e9f64053fa1bd
John Sebastian <32150363+sebastianschroh@users.noreply.github.com>
2019-04-06 16:46:56 -0600

    i don't know how to use git

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SpaceBook') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,13 +18,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+    .m-b-md {
+        font-size: 84px;
+        text-align: center;
+        margin-bottom: 30px;
+
+    }
+    </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'SpaceBook') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,6 +41,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                                
+                        <li class = "nav-item"><a class= "nav-link" href="default.asp">Home</a></li>
+                        <li class = "nav-item"><a class= "nav-link" href="default.asp">News</a></li>
+                        <li class = "nav-item"><a class= "nav-link" href="default.asp">Search</a></li>
+                        <li class = "nav-item"><a class= "nav-link" href="default.asp">Forum</a></li>
+                        <li class = "nav-item"><a class= "nav-link" href="default.asp">Picture of the Day</a></li>
 
                     </ul>
 
