7a65cd4f8cdb126bd430ceb06eeedb9fc45154b8
John Sebastian <32150363+sebastianschroh@users.noreply.github.com>
2019-04-08 20:48:00 -0600

    made search prettier

--- resources/views/Search/searchview.blade.php
+++ resources/views/Search/searchview.blade.php
@@ -18,6 +18,23 @@
         float: left;
         margin-right: 10px;
     }
+    .searchbar {
+        border-radius: 100px;
+        padding: 6px;
+        padding-left: 20px;
+        width: 1000px;
+        border: none;
+        margin-top: 12px;z
+        margin-right: 12px;
+        font-size: 18px;
+        text-indent: 1px;
+    }
+
+    .coolradiobutton{
+        border: none;
+        padding: 10px;
+        margin: 5px;
+    }
 
 @endsection
 
@@ -26,12 +43,12 @@
 <h1 class = "page_heading">Search the Cosmos (or just the site)</h1>                
 
 <form class ="searchArea" action="/results">
-Query: <br>
-<input type="text" name="query"><br><br>
-<input type="radio" name="type" value="News">News<br>
-<input type="radio" name="type" value="Forums">Forums<br>
-<input type="radio" name="type" value="Pictures">Pictures<br>
-<input type="submit" name="search">
+Keyword: <br>
+<input class="searchbar" type="text" name="query"><br>
+<input class="coolradiobutton" style="margin-top: 20px;" type="radio" name="type" value="News" checked>News<br>
+<input class="coolradiobutton" type="radio" name="type" value="Forums">Forums<br>
+<input class="coolradiobutton" type="radio" name="type" value="Pictures">Pictures<br>
+<input type="submit" style="margin-top: 20px; padding-left: 20px; padding-right: 20px;" name="search" value="Search">
 </form>
 </div>
 @endsection
