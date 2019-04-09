18a387eac5e3fd8275e068111bcf627dd7e534b8
BrandonKhoo <brandonkhoo.bk@gmail.com>
2019-04-09 13:03:30 -0600

    forum and view stuff

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -5,15 +5,25 @@
         color: lightgray;
     }
 
+    #rounded {
+      border-radius: 25px;
+      border: 2px solid green;
+      width: 600px;
+
+      padding: 10px;
+    }
+
     .thread {
-        background-color: #292b2d; 
+        background-color: #292b2d;
+        opacity: 0.9;
         color: lightgray;
         margin-top: 20px;
         margin-bottom: 20px;
         overflow: auto;
         padding: 10px;
+        border-radius: 25px;
     }
-    .thread:hover {background-color: #505050;}
+    .thread:hover {background-color: #505050; opacity: 1;}
 
     img {
         float: left;
@@ -24,12 +34,16 @@
 
 @section('content')
 <div class="fullscreen" style="background-image: url(nebula.jpg); padding: 5%;">
-<h1 class = "page_heading">Forum</h1>                
+<h1 class = "page_heading">Forum</h1>
 @foreach($threads as $thread)
 <a href="javascript:void(0)" style="text-decoration: none;"><div class = 'thread'>
             <h2>{{$thread->title}} <span style="float: right; font-size:10px;">Thread ID: {{$thread->str}} </span></h2>
             Submitted by User: {{$thread->getUser()}}
-            <br>
+              <br>
+              <br>
+            <div style=" opacity: 0.5;">
+            <h3 id = "rounded">{{$thread->getPaddedContent()}}<span style="float: right; font-size:4px;"></span><h3>
+            </div>
             <br>
         </div>
         </a>
